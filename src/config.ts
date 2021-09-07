39c98c7d47e33bf3511e37e838bc925160fb79c3
TenAceousSpade <66499590+TenAceousSpade@users.noreply.github.com>
2021-09-07 00:36:40 -0400

    Updated Configuration for Unraid

--- src/config.ts
+++ src/config.ts
@@ -4,15 +4,16 @@
 // https://github.com/Dev-CasperTheGhost/snaily-cadv3/wiki/Config-file
 const config = {
   port: Number(process.env.PORT) || 3030,
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "admin",
+  host: process.env.DB_HOST || "192.168.1.69",
+  user: process.env.DB_USER || "snaily-cad",
+  password: process.env.DB_PASSWORD || "snaily-cad",
   databaseName: process.env.DB_NAME || "snaily-cad",
   jwtSecret: process.env.JWT_SECRET || "bongo super cat",
   env: process.env.PROFILE || "production",
-  allowIframes: false, // true or false
-  secureCookie: false, // oNLY SET TO `true` if your site uses `https://`!
+  allowIframes: true, // true or false
+  secureCookie: true, // oNLY SET TO `true` if your site uses `https://`!
   databasePort: 3306,
 };
 
 export default config;
+
